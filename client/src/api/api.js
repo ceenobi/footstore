9369d8e8515cde696deda50ced0673ae90d6a001
ceenobi <cobimbachu@gmail.com>
2023-04-17 04:44:37 +0100

    fix api endpoints

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -6,43 +6,43 @@ import {
 } from '../config/baseURL'
 
 export const getFeaturedProducts = async () => {
-  const res = await instance.get('/products/featured')
+  const res = await instance.get('/api/products/featured')
   return res
 }
 export const getAllProducts = async () => {
-  const res = await instance.get('/products')
+  const res = await instance.get('/api/products')
   return res
 }
 export const getCategories = async () => {
-  const res = await instance.get('/categories')
+  const res = await instance.get('/api/categories')
   return res
 }
 export const getProductsByCategory = async (params) => {
-  const res = await instance.get(`/products/category/${params}`)
+  const res = await instance.get(`/api/products/category/${params}`)
   return res
 }
 export const getSingleProduct = async (params) => {
-  const res = await instance.get(`/products/${params}`)
+  const res = await instance.get(`/api/products/${params}`)
   return res
 }
 export const deleteProduct = async (params, config) => {
-  const res = await instance.delete(`/products/${params}`, config)
+  const res = await instance.delete(`/api/products/${params}`, config)
   return res
 }
 export const placeOrder = async (order, config) => {
-  const res = await instance.post('/orders', order, config)
+  const res = await instance.post('/api/orders', order, config)
   return res
 }
 export const getAllOrders = async (config) => {
-  const res = await instance.get('/orders', config)
+  const res = await instance.get('/api/orders', config)
   return res
 }
 export const getUserOrder = async (config) => {
-  const res = await instance.get('/orders/user', config)
+  const res = await instance.get('/api/orders/user', config)
   return res
 }
 export const getOrderDetail = async (params, config) => {
-  const res = await instance.get(`/orders/${params}`, config)
+  const res = await instance.get(`/api/orders/${params}`, config)
   return res
 }
 
@@ -51,19 +51,19 @@ export const cashOrder = async (orderId, order, config) => {
   return res
 }
 export const paypalOrder = async (orderId, order, config) => {
-  const res = await instance.put(`/orders/${orderId}/paypal`, order, config)
+  const res = await instance.put(`/api/orders/${orderId}/paypal`, order, config)
   return res
 }
 export const paypalDelivery = async (orderId, order, config) => {
   const res = await instance.put(
-    `/orders/${orderId}/paypaldelivery`,
+    `/api/orders/${orderId}/paypaldelivery`,
     order,
     config
   )
   return res
 }
 export const trackOrders = async (params, order, config) => {
-  const res = await instance.put(`/orders/${params}/tracking`, order, config)
+  const res = await instance.put(`/api/orders/${params}/tracking`, order, config)
   return res
 }
 
@@ -76,20 +76,20 @@ export const uploadCloudinary = async (file) => {
 }
 
 export const createNewProduct = async (product, config) => {
-  const newProduct = await instance.post('/products', product, config)
+  const newProduct = await instance.post('/api/products', product, config)
   return newProduct
 }
 
 export const getUserProfile = async (params, config) => {
-  const res = await instance.get(`/auth/profile/${params}`, config)
+  const res = await instance.get(`/api/auth/profile/${params}`, config)
   return res
 }
 export const updateUserProfile = async (profile, config) => {
-  const res = await instance.put('/auth/profile/', profile, config)
+  const res = await instance.put('/api/auth/profile/', profile, config)
   return res
 }
 
 export const getProductBySearch = async (searchParams) => {
-  const res = await instance.get(`/catalog/search/?q=${searchParams}`)
+  const res = await instance.get(`/api/catalog/search/?q=${searchParams}`)
   return res
 }
